fe3392df8b3b65aa74975667cc0353cd88adbaeb
Cosmin George Iacob <George.Iacob@orange.com>
2021-02-06 22:56:38 +0200

    add user cmd service add mongo db to query service

---
+++ user_cmd_api/src/test/java/cqrs/event/sourcing/example/user_cmd_api/UserCmdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cqrs.event.sourcing.example.user_cmd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCmdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
