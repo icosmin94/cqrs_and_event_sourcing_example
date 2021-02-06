6c50179ce4fe319a00db74ae5670a1fb2066029d
Cosmin George Iacob <George.Iacob@orange.com>
2021-02-07 01:34:54 +0200

    add user core module

---
+++ user_core/src/test/java/cqrs/event/sourcing/example/usercore/UserCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cqrs.event.sourcing.example.usercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
