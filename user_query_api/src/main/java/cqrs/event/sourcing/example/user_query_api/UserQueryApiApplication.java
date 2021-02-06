c914b0abecac71754dc08d174bce3dad9801a4ee
Cosmin George Iacob <George.Iacob@orange.com>
2021-02-06 22:50:49 +0200

    first commit

---
+++ user_query_api/src/main/java/cqrs/event/sourcing/example/user_query_api/UserQueryApiApplication.java
@@ -0,0 +1,13 @@
+package cqrs.event.sourcing.example.user_query_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserQueryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserQueryApiApplication.class, args);
+    }
+
+}
---
+++ user_query_api/src/test/java/cqrs/event/sourcing/example/user_query_api/UserQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cqrs.event.sourcing.example.user_query_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserQueryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
