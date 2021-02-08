45c50d16614dfbfe96037bb2437941141a3afefa
Cosmin George Iacob <George.Iacob@orange.com>
2021-02-08 09:28:16 +0200

    refactor modules

---
+++ user_core/src/main/java/cqrs/event/sourcing/example/usercore/models/User.java
@@ -0,0 +1,12 @@
+package cqrs.event.sourcing.example.usercore.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String gigi;
+}
